09cdc08aa9d8b593b12352de22ad16fcac4d4375
ZGrguric <zgrguric@xrplwin.com>
2022-08-27 19:10:58 +0200

    Use xplwin xrpl-orderbook-reader package

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,23 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Account;
-use \XRPLWin\XRPL\Client;
+use XRPLWin\XRPL\Client;
 use XRPLWin\XRPLOrderbookReader\LiquidityCheck;
 
 class MainController extends Controller
 {
     public function test()
     {
-        $client = new Client([]);
+        $client = app(Client::class);
 
         $lc = new LiquidityCheck([
             //'to' => ['currency' => 'XRP'],
-            //'from' => ['currency' => '534F4C4F00000000000000000000000000000000', 'issuer' => 'rsoLo2S1kiGeCcn6hCUXVrCpGMWLrRrLZz'],
-            'from' => ['currency' => 'MTA', 'issuer' => 'r95bSz69js5MrCoMdhejdGHvHyPRXumLTm'],
+            'from' => ['currency' => '534F4C4F00000000000000000000000000000000', 'issuer' => 'rsoLo2S1kiGeCcn6hCUXVrCpGMWLrRrLZz'],
+            //'from' => ['currency' => 'MTA', 'issuer' => 'r95bSz69js5MrCoMdhejdGHvHyPRXumLTm'],
             'to' => ['currency' => 'XRP'],
-            'amount' => 0.1,
-            'limit' => 500
-        ],[],$client);
+            'amount' => 0.1
+        ],[
+            'maxBookLines' => 500,
+
+        ],$client);
 
         $liquidity =  $lc->get();
 
