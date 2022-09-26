0999627c4b327654b1df8b52b64647c2bf71f144
ZGrguric <zgrguric@xrplwin.com>
2022-09-26 14:00:40 +0200

    currency formatterd

--- app/Models/DTransactionTrustset.php
+++ app/Models/DTransactionTrustset.php
@@ -9,4 +9,16 @@ namespace App\Models;
 final class DTransactionTrustset extends DTransaction
 {
   const TYPE = 3;
+
+  public function toArray()
+  {
+    $array = ['type' => $this::TYPE];
+    $array = \array_merge($array,parent::toArray());
+    if(isset($array['i'])){
+      //it is issued currency
+      $array['c_formatted'] = xrp_currency_to_symbol($array['c']);
+    }
+    return $array;
+  }
+  
 }
