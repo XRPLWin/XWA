925551ab13566670607ad6dc013bd8e8b346183c
zgrguric <zgrguric@xrplwin.com>
2023-10-13 21:44:47 +0200

    dUNL validators endpoint (from vl url)

--- app/Http/Controllers/Api/ValidatorController.php
+++ app/Http/Controllers/Api/ValidatorController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+#use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 use XRPL_PHP\Core\Buffer;
 use XRPL_PHP\Core\CoreUtilities;
@@ -35,36 +35,21 @@ class ValidatorController extends Controller
     foreach($data->validators as $v) {
       $validators[$v->validation_public_key] = [];
       if(\str_starts_with($v->validation_public_key,'ED')) {
-        $validators[$v->validation_public_key]['public_key_ed'] = $v->validation_public_key;  //EX
+        $validators[$v->validation_public_key]['public_key_ed'] = $v->validation_public_key;  //ED
         $validators[$v->validation_public_key]['public_key'] = $codec->encodeNodePublic(Buffer::from($v->validation_public_key)); //nX
         $validators[$v->validation_public_key]['address'] = CoreUtilities::deriveAddress(\strtoupper($v->validation_public_key)); //rX
       } else {
-        $validators[$v->validation_public_key]['public_key_ed'] = $v->validation_public_key; //nX
+        $validators[$v->validation_public_key]['public_key_ed'] = $codec->decodeNodePublic($v->validation_public_key)->toString(); //ED
         $validators[$v->validation_public_key]['public_key'] = $v->validation_public_key; //nX
-        $k = $v->validation_public_key;
-
-
-        $codec = new AddressCodec();
-        $edpubkey = $codec->decodeNodePublic('nHBbiP5ua5dUqCTz5i5vd3ia9jg3KJthohDjgKxnc7LxtmnauW7Z')->toString();
-        $rAddress = CoreUtilities::deriveAddress(\strtoupper($edpubkey));
-        dd($rAddress);
-
-
-        $validators[$v->validation_public_key]['address'] = CoreUtilities::deriveAddress(\strtoupper($v->validation_public_key)); //rX
-        dd($v,$validators[$v->validation_public_key] );
+        $validators[$v->validation_public_key]['address'] = CoreUtilities::deriveAddress(\strtoupper($validators[$v->validation_public_key]['public_key_ed'])); //rX
       }
-     
-      //$man = \bin2hex(\base64_decode($v->manifest));
-      //dd($validators[$v->validation_public_key],$man);
+
+      //Sequence:
+      $man = \bin2hex(\base64_decode($v->manifest));
+      $validators[$v->validation_public_key]['sequence'] = (int)\substr($man,2,8);
     }
-    //$test = CoreUtilities::deriveAddress(\strtoupper('ED1E88D64F134456B4BCBBC5554FDE292CCF8585DED2CAADAF83A499B4276BE312')); //get rAddress! from Ed25519
-    //$test2 = $codec->encodeNodePublic(Buffer::from('ED1E88D64F134456B4BCBBC5554FDE292CCF8585DED2CAADAF83A499B4276BE312'));
-    //ED1E88D64F134456B4BCBBC5554FDE292CCF8585DED2CAADAF83A499B4276BE312 to nHB2tHvDXE2GM3Cp9ivyAXU3NDLkf8mzYREQkcZ7wFJyBiaVLu24 ?
-    //$test = CoreUtilities::deriveAddress(\strtoupper('ED1E88D64F134456B4BCBBC5554FDE292CCF8585DED2CAADAF83A499B4276BE312'));
-    //dd($test,$test2);
-    //dd($url ,$data, \base64_decode($data->validators[0]->manifest));
-    //dd($data);
-    //dd($raw,$data);
+    $validators = \array_values($validators);
+    
     return response()->json([
       'url' => $url,
       'updated' => now(),
