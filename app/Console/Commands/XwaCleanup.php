6f4e470e6e391a351e6a6296e8fcf213160b6276
zgrguric <zgrguric@xrplwin.com>
2023-11-16 09:44:25 +0100

    Update XwaCleanup.php

--- app/Console/Commands/XwaCleanup.php
+++ app/Console/Commands/XwaCleanup.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class Banner extends Command
+class XwaCleanup extends Command
 {
     /**
      * The name and signature of the console command.
