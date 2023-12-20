b9c06013025d56cbf8a66e5455a3b1e5f0a94282
zgrguric <zgrguric@xrplwin.com>
2023-12-20 23:00:16 +0100

    [WIP] XwaAggrHookTransactions

--- app/Console/Commands/XwaAggrHookTransactions.php
+++ app/Console/Commands/XwaAggrHookTransactions.php
@@ -7,7 +7,10 @@ use App\Models\BHookTransaction;
 use App\Models\Synctracker;
 use App\Models\Tracker;
 use App\Models\MetricHook;
+use App\Models\BHook;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
+use App\Utilities\HookLoader;
 
 /**
  * This is scheduled job every 5 min, processes 1000 transactions
@@ -29,11 +32,15 @@ class XwaAggrHookTransactions extends Command
    */
   protected $description = 'Process and aggregate hook transactions';
 
+  /**
+   * A place to keep models in memory and referenced
+   */
+  private array $_mem_hookmodels = [];
   
   /**
    * How much transactions to process per job?
    */
-  private int $_limit = 1000; //1000
+  private int $_limit = 10000; //1000
 
   public function handle()
   {
@@ -69,16 +76,21 @@ class XwaAggrHookTransactions extends Command
       #DB::connection()->enableQueryLog();
       DB::beginTransaction();
       $i = 0;
-      $prevtx = null;
+      //$prevtx = null;
       foreach($txs as $tx) {
         if($txs->count() == $this->_limit && $tx->l == $reject_l) break; //last l we do not process this batch if dataset is full
         $this->handleUninstalledHooks($tx);
-        $this->aggrTx($tx,$prevtx);
+        $this->aggrTx($tx);
         $last_processed_ledger_index = $tx->l;
-        $prevtx = $tx;
+        //$prevtx = $tx;
         $i++;
       }
       $this->postProcessMetric();
+
+      //Exec save on all loaded hook models
+      foreach($this->_mem_hookmodels as $hm) {
+        $hm->save();
+      }
       $this->info('Processed '.$i.' hook transactions last li: '.$last_processed_ledger_index);
       Tracker::saveInt('aggrhooktx',$last_processed_ledger_index);
       DB::commit();
@@ -122,16 +134,21 @@ class XwaAggrHookTransactions extends Command
    * Collect and store statistics
    * Store aggragation (daily) data to metric_hooks table.
    * @param BHookTransaction $tx - transaction to process
-   * @param ?BHookTransaction $prevtx - transaction that was processed previously
    * @return void
    */
-  private function aggrTx(BHookTransaction $tx,?BHookTransaction $prevtx): void
+  private function aggrTx(BHookTransaction $tx): void
   {
-    $metric = MetricHook::where('hook',$tx->hook)->whereDay('day', $tx->t)->first();
+    $hookDef = $this->_getHookModel($tx->hook,$tx->l);
+    if(!$hookDef)
+      throw new \Exception('Unable to find hook definiton in aggrTx '.$tx->hook.' li:'.$tx->l);
+
+    $metric = MetricHook::where('hook',$tx->hook)->where('l',$hookDef->l_from)->whereDay('day', $tx->t)->first();
+    
     if(!$metric)
     {
       $metric = new MetricHook;
       $metric->hook = $tx->hook;
+      $metric->l = $hookDef->l_from; //hook version
       $metric->day = $tx->t;
     }
 
@@ -159,17 +176,25 @@ class XwaAggrHookTransactions extends Command
     $metric->save();
   }
 
+  /**
+   * Runs once per job, handles metrics with is_processed=0
+   */
   private function postProcessMetric()
   {
-    $metrics = MetricHook::select('id','hook','day')->where('is_processed', false)->orderBy('id','asc')->limit(1000)->get();
+    $metrics = MetricHook::select('id','hook','l','day')->where('is_processed', false)->orderBy('id','asc')->limit(1000)->get();
     foreach($metrics as $metric)
     {
+      $hookDef = $this->_getHookModel($metric->hook,$metric->l);
+      if(!$hookDef)
+        throw new \Exception('Unable to find hook definiton in postProcessMetric '.$metric->hook.' li:'.$metric->l);
       //Count all rows where hookaction = 3 (install) but not 34 (install and uninstall)
       $countInstalls = BHookTransaction::repo_count(
         [
           ['hook',$metric->hook],
           ['hookaction',[3,34]],
           ['tcode','tesSUCCESS'],
+          ['l','>=',$metric->l],
+          //todo end range l from hookDef 
           //['t','>=',$Ymd.' 00:00:00'],
           ['t','<=',$metric->day->format('Y-m-d').' 23:59:59'],
         ]
@@ -179,6 +204,7 @@ class XwaAggrHookTransactions extends Command
           ['hook',$metric->hook],
           ['hookaction',4],
           ['tcode','tesSUCCESS'],
+          ['l','>=',$metric->l],
           //['t','>=',$Ymd.' 00:00:00'],
           ['t','<=',$metric->day->format('Y-m-d').' 23:59:59'],
         ]
@@ -190,6 +216,28 @@ class XwaAggrHookTransactions extends Command
       $metric->is_processed = true;
       $metric->save();
       unset($count);
+
+      //Part 2 - update Hook hooks.stat_* fields - all affected hooks
+      //foreach($affectedHooks as $affectedHook) {
+      //  $HookDef = $this->_getHookModel($affectedHook,)
+      //}
     }
+  }
+
+
+  private function _getHookModel($hook, $ledger_index): ?BHook
+  {
+    Cache::tags(['hook'.$hook])->flush();
+    
+    $k = $hook.'_'.$ledger_index;
+    if(!isset($this->_mem_hookmodels[$k])) {
+      $hm = HookLoader::get($hook,$ledger_index,false);
+      if(!$hm) {
+        //load closest to the provided ledger_index
+        $hm = HookLoader::getClosestByHash($hook,$ledger_index);
+      }
+      $this->_mem_hookmodels[$k] = $hm;
+    }
+    return $this->_mem_hookmodels[$k];
   }
 }
