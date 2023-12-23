40616fc45981c70c7143b6d01fffc39fe8ba7ab8
zgrguric <zgrguric@xrplwin.com>
2023-12-23 20:02:43 +0100

    Update XRPAddress.php

--- app/Rules/XRPAddress.php
+++ app/Rules/XRPAddress.php
@@ -21,23 +21,4 @@ class XRPAddress implements ValidationRule
     }
     //todo add base58 check here https://github.com/furqansiddiqui/base58check-php
   }
-  
-  /**
-   * Run the validation rule.
-   *
-   * @param  string  $attribute
-   * @param  mixed  $value
-   * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
-   * @return void
-   */
-  /*public function __invoke($attribute, $value, $fail)
-  {
-      $len = strlen($value);
-      //dd($len > 25,$len < 35, ($len > 25 && $len < 35));
-      if(!($len > 25 && $len < 35)) {
-          $fail('The :attribute is not valid XRP Address format.');
-      }
-
-      //todo add base58 check here https://github.com/furqansiddiqui/base58check-php
-  }*/
 }
