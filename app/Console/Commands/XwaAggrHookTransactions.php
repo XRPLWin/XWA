256cdcb7d0f6ee19adb30633873c37a00b802930
zgrguric <zgrguric@xrplwin.com>
2024-03-18 08:49:35 +0100

    Update XwaAggrHookTransactions.php

--- app/Console/Commands/XwaAggrHookTransactions.php
+++ app/Console/Commands/XwaAggrHookTransactions.php
@@ -40,7 +40,7 @@ class XwaAggrHookTransactions extends Command
   /**
    * How much transactions to process per job?
    */
-  private int $_limit = 5000; //5000
+  private int $_limit = 55000; //5000
 
   public function handle()
   {
