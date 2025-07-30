d87f4b4d24e96c7fbd96c18ccc84dd53dee4ad90
zgrguric <zgrguric@xrplwin.com>
2025-07-30 09:42:00 +0200

    Update xrpl.php

--- config/xrpl.php
+++ config/xrpl.php
@@ -161,9 +161,11 @@ return [
     'genesis_ledger_close_time' => env('XRPL_GENESIS_LEDGER_CLOSE_TIME', 751983661),
 
     //for connection via php GuzzleHttp (reporting server)
-    'rippled_server_uri' => 'https://xahau.network/explorer/xrplwin',
+    //'rippled_server_uri' => 'https://xahau.network/explorer/xrplwin',
+    'rippled_server_uri' => 'https://xahau.network',
     //for connection via php GuzzleHttp (full history server)
-    'rippled_fullhistory_server_uri' => 'https://xahau.network/explorer/xrplwin',
+    //'rippled_fullhistory_server_uri' => 'https://xahau.network/explorer/xrplwin',
+    'rippled_fullhistory_server_uri' => 'https://xahau.network',
     //websocket domain (example: 'xahau.network')
     'server_wss' => 'xahau.network',
     //'server_wss_syncer' => ['wss://xahau.network'],
