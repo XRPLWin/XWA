426f00741da5098e51a6ad94a71a8b8e98e9e1b1
zgrguric <zgrguric@xrplwin.com>
2026-08-28 23:38:19 +0200

    Enable unlreportsync

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,11 +25,11 @@ class Kernel extends ConsoleKernel
         ;
 
     //disable 28 08 2026
-    /*$schedule->command('xwa:unlreportssync')
+    $schedule->command('xwa:unlreportssync')
       ->withoutOverlapping(4) //lock expires every 4 mins, flag ledgers are approx every 12 mins
       ->everyFiveMinutes()
       ->onOneServer()
-      ->runInBackground();*/
+      ->runInBackground();
 
     $schedule->command('xwa:startsyncer')
       ->withoutOverlapping(11) //lock expires every 21 min (adjust times in commands also)
